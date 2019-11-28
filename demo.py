166684eabff078558c7c06a4f3f95444d0233604
Ashwin-Ramesh2607 <ashwin.cuber26@gmail.com>
2019-11-28 12:07:55 +0530

    Improved overlap for wait time

--- demo.py
+++ demo.py
@@ -150,12 +150,14 @@ def main(yolo):
                 latest_frame[track.track_id] = frame_count
                 cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
                 wait_time = round((queue_track_dict[track.track_id] / Input_FPS), 2)
+                cv2.putText(frame, str(track.track_id) + "->Time:" + str(wait_time) + " seconds", (int(bbox[0]), int(bbox[1])), 0, 0.8, (0, 0, 0), 3)
                 cv2.putText(frame, str(track.track_id) + "->Time:" + str(wait_time) + " seconds", (int(bbox[0]), int(bbox[1])), 0, 0.8, (0, 255, 77), 2)
 
             if alley_point_test == 'inside': # Alley Region
                 alley_track_dict[track.track_id] += 1
                 latest_frame[track.track_id] = frame_count
                 cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+                cv2.putText(frame, str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 0.8, (0, 0, 0), 3)
                 cv2.putText(frame, str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 0.8, (0, 255, 77), 2)
 
             if track.track_id in store_track_dict: # Entire Store Area
